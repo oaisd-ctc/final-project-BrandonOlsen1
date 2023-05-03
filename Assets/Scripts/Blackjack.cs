5d10d11fb66cd8da6dca6f9ca92f907d8b997fda
Brandon Olsen <112421088+BrandonOlsen1@users.noreply.github.com>
2023-05-03 13:59:32 -0400

    game updates

--- Assets/Scripts/Blackjack.cs
+++ Assets/Scripts/Blackjack.cs
@@ -15,6 +15,12 @@ public class Blackjack : MonoBehaviour
     public GameObject dealerPos;
 
 
+    public int playertotal;
+    
+
+    public int cardpointcount;
+
+
 
 
 
@@ -24,21 +30,7 @@ public class Blackjack : MonoBehaviour
 
     public static string[] suits = new string[] { "C", "D", "H", "S" };
     public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-
-
-
-
-
-
-
-
-
-
-
-
     public List<string> deck;
-
-
 
     void Start()
     {
@@ -54,35 +46,52 @@ public class Blackjack : MonoBehaviour
     }
 
 
-    // public string GetValue(string card)
-    // {
-    //     switch (card)
-    //    {
-    //     // turn jacks value to 10
-    //     case 1: 
-            
-    //     break;
+    public int GetValue(string card)
+    {
+        int value = 0;
 
-    //     // turn Queens value to 10
-    //     case 2:
+        if (card[1] == '2' || card[1] == '3' || card[1] == '4' || card[1] == '5' || card[1] == '6' || card[1] == '7' || card[1] == '8' || card[1] == '9')
+        {
+            value += System.Convert.ToInt32(System.Convert.ToString(card[1]));
+        }
+        else if (card[1] == 'J' || card[1] == 'Q' || card[1] == 'K' || card[1] == 'A')
+        {
+            switch (card[1])
+            {
+                // turn jacks value to 10
+                case 'J':
+                    value += 10;
+                    break;
+                // turn Queens value to 10
+                case 'Q':
+                    value += 10;
+                    break;
 
-    //     break;
+                // turn Kings value to 10
+                case 'K':
+                    value += 10;
+                    break;
 
-    //     // turn Kings value to 10
-    //     case 3:
+                case 'A':
+                if (cardpointcount <= 21)
+                {
+                    value += 11;
+                }
+                else
+                {
+                    value += 1;
+                }
+                    break;
+            }
 
-    //     break;
+        }
+        else
+        {
+            value += 10;
+        }
 
-    //     else 
-        
-    //     //return the value of the integer
-    //     return value of int
-
-
-    //    }
-
-
-    // }
+        return value;
+    }
 
 
     public void PlayCards()
@@ -90,8 +99,8 @@ public class Blackjack : MonoBehaviour
         deck = GenerateDeck();
         Shuffle(deck);
         BlackjackDeal();
-        
-        
+
+
     }
 
 
@@ -134,9 +143,13 @@ public class Blackjack : MonoBehaviour
     void BlackjackDeal()
     {
         float xOffset = 0f;
+        playertotal = 0;
+        int playervalueone = 0;
+        int playervaluetwo = 0;
 
         for (int i = 0; i < 4; i++)
         {
+
             string card = deck[i];
             float positionx = playingPos.transform.position.x;
             float positiony = playingPos.transform.position.y;
@@ -147,25 +160,54 @@ public class Blackjack : MonoBehaviour
 
             newCard.GetComponent<Selectable>().faceUp = true;
 
-            // GetValue(card);
-            
-        i++;
+            GetValue(card);
 
-            string card1 = deck[i];
-            float positionx1 = dealerPos.transform.position.x;
-            float positiony1 = dealerPos.transform.position.y;
-            GameObject Dealercard = Instantiate(cardPrefab, new Vector2(positionx1 + xOffset, positiony1), Quaternion.identity);
 
-            Dealercard.name = card1;
+            if (i < 1)
+            {
+                GetValue(card);
+                playervalueone =+ GetValue(card);
+            }
+            else if (i > 1 || i < 3)
+            {
+                GetValue(card);
+                playervaluetwo =+ GetValue(card);
 
-            Dealercard.GetComponent<Selectable>().faceUp = true;
-            xOffset += 0.5f;
+                playertotal = playervalueone + playervaluetwo;
+                Debug.Log(playertotal);
+            }
+        
+                
 
-            // GetValue(card1);
+            i++;
 
+            if (i <= 2)
+            {
+                string card1 = deck[i];
+                float positionx1 = dealerPos.transform.position.x;
+                float positiony1 = dealerPos.transform.position.y;
+                GameObject Dealercard = Instantiate(cardPrefab, new Vector2(positionx1 + xOffset, positiony1), Quaternion.identity);
+
+                Dealercard.name = card1;
+
+
+
+
+
+                Dealercard.GetComponent<Selectable>().faceUp = true;
+
+
+                xOffset += 0.5f;
+
+                GetValue(card1);
+
+                int DealerValueone = GetValue(card1);
+            }
+
+            i++;
         }
 
-        
+
 
 
 
